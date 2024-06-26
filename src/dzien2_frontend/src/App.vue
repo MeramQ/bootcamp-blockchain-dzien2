aa74fc838242850c320baaeb02802401bcb968e3
Marcin Lesiak <marcinui07@gmail.com>
2024-06-26 19:17:03 +0200

    added better styling

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -3,7 +3,7 @@ import Blog from './components/Blog.vue';
 </script>
 
 <template>
-  <main>
+  <main class="container mx-auto">
     <Blog />
   </main>
 </template>
--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
+        </div>
     </div>
 </template>
 
 <script>
-import {dzien2_backend} from 'declarations/dzien2_backend/index'
+import { dzien2_backend } from 'declarations/dzien2_backend/index'
 export default {
     data() {
         return {
@@ -24,6 +32,9 @@ export default {
         async dodajWpisy() {
             await dzien2_backend.dodaj_wpis(this.nowyBlog);
         }
+    },
+    async mounted() {
+        this.pobierzWpisy();
     }
 }
 </script>
